86f1345821236835cc0eeea5305afc780a44cc64
dengyuting <17791537@qq.com>
2019-12-05 17:39:32 +0800

    test

--- sendmsg.py
+++ sendmsg.py
@@ -11,6 +11,7 @@ BUILD_NUMBER = str(os.getenv("BUILD_NUMBER"))
 JOB_NAME = str(os.getenv("JOB_NAME"))
 CAUSE = str(os.getenv("CAUSE"))
 BUILD_URL = str(os.getenv("BUILD_URL"))
+JOB_URL = str(os.getenv("JOB_URL"))
 
 path = os.path.dirname(__file__)
 summary_file = os.path.join(path, 'logs', 'ability_suites.summary.json')
@@ -37,7 +38,7 @@ def sendinfo():
     data['text'] = {}
     data['text']['content'] = '【ability接口自动化测试-测试环境】\n 测试执行结果：'+r[0]+ \
                               '\n TOTAL：' +str(r[1]) +'\n SUCCESS：' +str(r[2])+ '\n FAIL：' +str(r[3])+ \
-                              '\n 查看控制台：' +BUILD_URL+'console \n 测试报告地址：' +BUILD_URL+'Ability_20Test_20Report/ \n'
+                              '\n 查看控制台：' +BUILD_URL+'console \n 测试报告地址：' +JOB_URL+'Ability_20Test_20Report/ \n'
     # data['text']['mentioned_mobile_list'] = ["13559112969","@all"]
 
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=b8028a75-89ff-4dc4-a1a7-6d8e3fd44552'
