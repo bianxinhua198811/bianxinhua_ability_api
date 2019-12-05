6dcadbc0c1ab5b097461abc3b8022a1a2af986b1
dengyuting <17791537@qq.com>
2019-12-05 17:36:26 +0800

    test

--- sendmsg.py
+++ sendmsg.py
@@ -11,7 +11,6 @@ BUILD_NUMBER = str(os.getenv("BUILD_NUMBER"))
 JOB_NAME = str(os.getenv("JOB_NAME"))
 CAUSE = str(os.getenv("CAUSE"))
 BUILD_URL = str(os.getenv("BUILD_URL"))
-BUILD_USER = str(os.getenv("BUILD_USER"))
 
 path = os.path.dirname(__file__)
 summary_file = os.path.join(path, 'logs', 'ability_suites.summary.json')
@@ -26,7 +25,8 @@ def getresult():
                 total = jsonfile["stat"]["testcases"]["total"]
                 success = jsonfile["stat"]["testcases"]["success"]
                 fail = jsonfile["stat"]["testcases"]["fail"]
-                return result, total, success, fail
+                teststeps = jsonfile["stat"]["teststeps"]
+                return result, total, success, fail, teststeps
     except Exception as err:
         print("this is err:{}" .format(err))
 
@@ -35,9 +35,9 @@ def sendinfo():
     data = {}
     data['msgtype'] = 'text'
     data['text'] = {}
-    data['text']['content'] = '【ability接口自动化测试-测试环境】 \n 构建人：' +BUILD_USER+ \
-                              '\n 测试执行结果：'+r[0]+ '\n TOTAL：' +str(r[1]) +'\n SUCCESS:' +str(r[2])+ '\n FAIL:' +str(r[3])+ \
-                              '\n 查看控制台：' +BUILD_URL+'console \n 测试报告地址：XXXXXXXX \n'
+    data['text']['content'] = '【ability接口自动化测试-测试环境】\n 测试执行结果：'+r[0]+ \
+                              '\n TOTAL：' +str(r[1]) +'\n SUCCESS：' +str(r[2])+ '\n FAIL：' +str(r[3])+ \
+                              '\n 查看控制台：' +BUILD_URL+'console \n 测试报告地址：' +BUILD_URL+'Ability_20Test_20Report/ \n'
     # data['text']['mentioned_mobile_list'] = ["13559112969","@all"]
 
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=b8028a75-89ff-4dc4-a1a7-6d8e3fd44552'
