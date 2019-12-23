4cf293ebf90bee10a4a0a5c8fd29a332cddeeb12
dengyuting <17791537@qq.com>
2019-12-23 16:57:42 +0800

    test

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # @Time     :2019/10/12 11:25
 # @Author   :dengyuting
 # @File     :run.py
+import requests
+
+# s = requests.Session()
+
+r = requests.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
+print(r.text)
+r = requests.get('https://httpbin.org/cookies')
+print(r.text)
+
+
+
+s = requests.Session()
+
+r = s.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
+print(r.text)
+r = s.get('https://httpbin.org/cookies')
+print(r.text)
